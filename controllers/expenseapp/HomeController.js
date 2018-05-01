8892f24a6cfc77ec929cd3e4c390d0660fb29d0a
Tonmoy <devatone.lancer@gmail.com>
2018-05-02 01:48:21 +0530

    add new controller

---
+++ controllers/expenseapp/HomeController.js
@@ -0,0 +1,5 @@
+module.exports = {
+	index : function(req, res) {
+		res.send("<h1 style='text-align:center'>Welcome To BinsSoft Node API</h1>");
+	}
+}
--- systems/controllers.js
+++ systems/controllers.js
@@ -1,7 +1,39 @@
 var Controller = {};
-const filePath  = './controllers'
+const filePath  = './controllers';
 global.fs.readdirSync(filePath).forEach(file => {
-	file = file.slice(0,-3);
-	Controller[file] = require("."+filePath+"/"+file);
+	if (fs.lstatSync(filePath+"/"+file).isDirectory() ) {
+		Controller[file]={};
+		global.fs.readdirSync(filePath+"/"+file).forEach(subfile => {
+
+			if (fs.lstatSync(filePath+"/"+file+"/"+subfile).isDirectory() ) {
+				Controller[file][subfile]={};
+				global.fs.readdirSync(filePath+"/"+file+"/"+subfile).forEach(subsubfile => {
+
+
+
+					if (fs.lstatSync(filePath+"/"+file+"/"+subfile+"/"+subsubfile).isDirectory() ) {
+						Controller[file][subfile][subsubfile]={};
+						global.fs.readdirSync(filePath+"/"+file+"/"+subfile+"/"+subsubfile).forEach(subsubsubfile => {
+								subsubsubfile = subsubsubfile.slice(0,-3);
+								Controller[file][subfile][subsubfile][subsubsubfile] = require("."+filePath+"/"+file+"/"+subfile+"/"+subsubfile+"/"+subsubsubfile);
+						});
+					} else {
+						subsubfile = subsubfile.slice(0,-3);
+						Controller[file][subfile][subsubfile] = require("."+filePath+"/"+file+"/"+subfile+"/"+subsubfile);
+					}
+	
+				})
+			} else {
+				subfile = subfile.slice(0,-3);
+				Controller[file][subfile] = require("."+filePath+"/"+file+"/"+subfile);
+			}
+			
+		})
+	} else {
+		file = file.slice(0,-3);
+		Controller[file] = require("."+filePath+"/"+file);	
+	}
+	
 })
+console.log(JSON.stringify(Controller));
 module.exports =  Controller;
