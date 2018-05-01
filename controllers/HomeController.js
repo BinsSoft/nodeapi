484e72016a3b4a08a151a4fbd6d77b721f4a3e1d
Tonmoy <devatone.lancer@gmail.com>
2018-05-02 01:02:26 +0530

    add default controller

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,5 +1,5 @@
 module.exports = {
 	index : function(req, res) {
-		res.send("<h1 style='text-align:center'>Welcoome To BinsSoft Node API</h1>");
+		res.send("<h1 style='text-align:center'>Welcome To BinsSoft Node API</h1>");
 	}
 }
