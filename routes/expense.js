6175f5e8c5c875a757a105e96c546821aad120b5
tonmoymatrix <tonmoy@matrixnmedia.com>
2018-05-16 19:35:19 +0530

    change the origin

--- routes/expense.js
+++ routes/expense.js
@@ -8,7 +8,7 @@ global.app.use(function(req, response, next) {
    response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Credentials", "true");
     response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    response.setHeader("Access-Control-Allow-Headers", "access-control-allow-origin,content-type");
+    response.setHeader("Access-Control-Allow-Headers", "access-control-allow-credentials,access-control-allow-headers,access-control-allow-methods,access-control-allow-origin,content-type");
   return next();
 });
 let prefix = process.env.EXPANSE_APP;
