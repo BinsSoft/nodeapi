85ae483878a14231cc6c27fd164615ffd66afb8c
tonmoymatrix <tonmoy@matrixnmedia.com>
2018-07-13 19:43:49 +0530

    work on live status

--- controllers/train/ApiController.js
+++ controllers/train/ApiController.js
@@ -6,9 +6,7 @@ var request 		= require('request');
 module.exports = {
 
 	trainDetails(req, res) {
-		//res.send(req.body);
-		//https://api.railbeeps.com/api/fetchAvailability/api-key/web-cf1de6ecb6d81d7fdbbd4a0ee20df4d5/trainno/3159/from/3440/to/312/doj/Thu,%2012th%20Jul
-
+		
 		let apiUrl = api.url+'fetchAvailability/api-key/web-'+ api.key+'/trainno/'+req.body.trainid+'/from/'+req.body.source+'/to/'+req.body.dest+'/doj/'+global.moment().format('YYYY-MM-DD');
 		//let apiUrl = 'http://testyourprojects.biz/custom/test_tonmoy/train.json';
 		request(apiUrl, function(error, response){
@@ -23,6 +21,7 @@ module.exports = {
 
 	trainNameSearch(req, res) {
 		let searchQuery = req.query.s;
+		searchQuery = searchQuery.toUpperCase();
 		const file = 'public/train/stations.json';
 		global.fs.readFile(file, 'utf8', function (err, stations){
 			stations = JSON.parse(stations);
@@ -34,9 +33,9 @@ module.exports = {
 	},
 
 	trainBetweenStation(req, res) {
-		let apuUrl = api.url+'getTrainsBetweenStations/api-key/web-'+ api.key+'/from/'+req.body.source+'/to/'+req.body.destination+'/date/'+global.moment().format('YYYY-MM-DD')+'/sortby/departure/orderby/ASC/search/yes';
+		let apiUrl = api.url+'getTrainsBetweenStations/api-key/web-'+ api.key+'/from/'+req.body.source+'/to/'+req.body.destination+'/date/'+global.moment().format('YYYY-MM-DD')+'/sortby/departure/orderby/ASC/search/yes';
 
-		request(apuUrl, function(error, response){
+		request(apiUrl, function(error, response){
 			let data = JSON.parse(response.body);
 			res.send({
 				status : 1,
@@ -44,5 +43,19 @@ module.exports = {
 			});
 		})
 		
+	},
+
+	trainCurrentStatus(req, res) {
+		//let apiUrl = api.url+'getRunningStatus/api-key/web-'+ api.key+'/trainno/'+req.body.trainid+'/date/'+global.moment().format('YYYY-MM-DD');
+		let apiUrl = 'http://testyourprojects.biz/custom/test_tonmoy/train.json';
+		
+		request(apiUrl, function(error, response){
+			let data = JSON.parse(response.body);
+			res.send({
+				status : 1,
+				body : data
+			});
+		})
 	}
+	//https://api.railbeeps.com/api/getRunningStatus/api-key/web-cf1de6ecb6d81d7fdbbd4a0ee20df4d5/trainno/3159/date/Thu,%2012th%20Jul
 }
--- routes/train.js
+++ routes/train.js
@@ -6,5 +6,5 @@ let prefix = '/train-api/';
 router.post(prefix+'train-details/', Controller.train.ApiController.trainDetails);
 router.get(prefix+'train-auto-search', Controller.train.ApiController.trainNameSearch);
 router.post(prefix+'train-between-stations', Controller.train.ApiController.trainBetweenStation);
-
+router.post(prefix+'train-current-status', Controller.train.ApiController.trainCurrentStatus);
 module.exports = router;
